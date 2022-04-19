1e6ab5abdee3e587ff87053dc2e9022f309a8d6f
LAPTOP-QS1LABDN\quan6 <qhuynh@vhslab.com>
2022-04-18 22:38:26 -0400

    fix typo build

--- components/admin/quest/index.js
+++ components/admin/quest/index.js
@@ -3,4 +3,4 @@ export { default as TwitterAuthQuest } from "./TwitterAuthQuest";
 export { default as TwitterFollowQuest } from "./TwitterFollowQuest";
 export { default as AnomuraSeeFoodQuest } from "./AnomuraSeeFoodQuest";
 export { default as InstagramFollowQuest } from "./InstagramFollowQuest";
-export { default as TwitterRetweetQuest } from "./TwitterReTweetQuest";
+export { default as TwitterRetweetQuest } from "./TwitterRetweetQuest";

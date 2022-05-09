4f93dd695d3acf94612636de44e15d560bff9617
LAPTOP-QS1LABDN\quan6 <qhuynh@vhslab.com>
2022-05-09 14:05:06 -0400

    fixing thumbnail

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -1,8 +1,10 @@
 import { NextResponse, NextRequest } from "next/server";
 export async function middleware(req, ev) {
     const { pathname } = req.nextUrl;
-    // if (pathname == "/") {
-    //     return NextResponse.redirect(`${process.env.NEXT_PUBLIC_WEBSITE_HOST}/user/quest`);
+    // if (pathname == "/" || pathname == "/challenger") {
+    //     return NextResponse.redirect(
+    //         `${process.env.NEXT_PUBLIC_WEBSITE_HOST}/challenger/user/quest`
+    //     );
     // }
     return NextResponse.next();
 }

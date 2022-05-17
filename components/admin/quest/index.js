79a4b22d984247f00e8da146a1a3ea5ec2c6f027
crowhound <jonathan.westfall1@gmail.com>
2022-05-17 11:53:41 -0500

    Added Twitter Space Import for the component index.

--- components/admin/quest/index.js
+++ components/admin/quest/index.js
@@ -4,3 +4,4 @@ export { default as TwitterFollowQuest } from "./Forms/TwitterFollowQuest";
 export { default as AnomuraSeeFoodQuest } from "./Forms/AnomuraSeeFoodQuest";
 export { default as InstagramFollowQuest } from "./Forms/InstagramFollowQuest";
 export { default as TwitterRetweetQuest } from "./Forms/TwitterRetweetQuest";
+export { default as TwitterSpaceCodeQuest } from "./Forms/TwitterSpaceQuest";

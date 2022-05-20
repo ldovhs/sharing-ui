1a049612925e0565259f0a39c9b465c43ca13c35
LAPTOP-QS1LABDN\quan6 <qhuynh@vhslab.com>
2022-05-20 14:32:51 -0400

    test analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import React, { StrictMode } from "react";
+import React, { StrictMode, useEffect } from "react";
 import "../styles/globals.css";
 import "../sass/admin/adminBootstrap.css";
 import { Web3Provider } from "@context/Web3Context";
@@ -6,14 +6,44 @@ import { SessionProvider } from "next-auth/react";
 import { AdminGuard } from "containers/admin/AdminGuard";
 import { QueryClient, QueryClientProvider, useQuery } from "react-query";
 import Enums from "enums";
-// basePath={`/challenger/api/auth`}
+import Script from "next/script";
+import * as gtag from "../lib/ga/gtag";
+import { useRouter } from "next/router";
+
 const queryClient = new QueryClient();
+
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
+    const router = useRouter();
+    useEffect(() => {
+        const handleRouteChange = (url) => {
+            gtag.pageview(url);
+        };
+        router.events.on("routeChangeComplete", handleRouteChange);
+        return () => {
+            router.events.off("routeChangeComplete", handleRouteChange);
+        };
+    }, [router.events]);
+
     return (
         <SessionProvider session={session} basePath={`/challenger/api/auth`}>
             <Web3Provider>
                 <QueryClientProvider client={queryClient}>
                     <StrictMode>
+                        <Script
+                            strategy="lazyOnload"
+                            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+                        />
+
+                        <Script strategy="lazyOnload">
+                            {`
+                                window.dataLayer = window.dataLayer || [];
+                                function gtag(){dataLayer.push(arguments);}
+                                gtag('js', new Date());
+                                gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                                page_path: window.location.pathname,
+                                });
+                            `}
+                        </Script>
                         {Component.requireAdmin ? (
                             <Component.Layout>
                                 <AdminGuard>

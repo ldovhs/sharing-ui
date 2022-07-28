7baa2e7cffc592e97ba478c9da319dd05b72e011
LAPTOP-QS1LABDN\quan6 <qhuynh@vhslab.com>
2022-07-28 13:06:59 -0400

    disable user register

--- pages/api/user/signup.js
+++ pages/api/user/signup.js
@@ -11,34 +11,35 @@ export default async function whitelistSignUp(req, res) {
             try {
                 console.log(`**Sign up new user**`);
                 const { address, signature } = req.body;
-                if (!signature || !address) {
-                    return res
-                        .status(200)
-                        .json({ isError: true, message: "Missing user info for sign up." });
-                }
+                return res.status(200).json({ isError: true, message: "ok" });
+                // if (!signature || !address) {
+                //     return res
+                //         .status(200)
+                //         .json({ isError: true, message: "Missing user info for sign up." });
+                // }
 
-                let wallet = utils.getAddress(address);
-                let isValid = utils.isAddress(address);
-                if (!wallet || !isValid) {
-                    return res
-                        .status(200)
-                        .json({ isError: true, message: "The wallet address is not valid" });
-                }
+                // let wallet = utils.getAddress(address);
+                // let isValid = utils.isAddress(address);
+                // if (!wallet || !isValid) {
+                //     return res
+                //         .status(200)
+                //         .json({ isError: true, message: "The wallet address is not valid" });
+                // }
 
-                const existingUser = await getWhiteListUserByWallet(wallet);
-                if (existingUser) {
-                    return res.status(200).json({ existingUser });
-                }
+                // const existingUser = await getWhiteListUserByWallet(wallet);
+                // if (existingUser) {
+                //     return res.status(200).json({ existingUser });
+                // }
 
-                const newUser = await addNewUser(wallet);
-                if (!newUser) {
-                    return res.status(200).json({
-                        isError: true,
-                        message: "Cannot sign up new user. Please contact administrator!",
-                    });
-                }
+                // const newUser = await addNewUser(wallet);
+                // if (!newUser) {
+                //     return res.status(200).json({
+                //         isError: true,
+                //         message: "Cannot sign up new user. Please contact administrator!",
+                //     });
+                // }
 
-                res.status(200).json(newUser);
+                // res.status(200).json(newUser);
             } catch (error) {
                 console.log(error);
                 return res.status(200).json({ isError: true, message: error.message });

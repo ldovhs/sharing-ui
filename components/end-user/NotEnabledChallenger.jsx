d445f486c014c8cdfa0ea4dccc8f4b6b12db8526
LAPTOP-QS1LABDN\quan6 <qhuynh@vhslab.com>
2022-10-22 15:07:52 -0400

    refactore a few places

--- components/end-user/NotEnabledChallenger.jsx
+++ components/end-user/NotEnabledChallenger.jsx
@@ -56,15 +56,3 @@ const NotEnabledChallenger = ({ session }) => {
 };
 
 export default NotEnabledChallenger;
-
-import { unstable_getServerSession } from "next-auth/next";
-import { authOptions } from "pages/api/auth/[...nextauth]";
-export async function getServerSideProps(context) {
-    const session = await unstable_getServerSession(context.req, context.res, authOptions);
-
-    return {
-        props: {
-            session,
-        },
-    };
-}
